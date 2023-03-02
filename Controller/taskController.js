495231e64af209e3ccba52458b70434dade3e880
bhautik <bhautik.zanzrukiya@coodeit.com>
2023-03-02 14:05:32 +0530

    -formate

--- Controller/taskController.js
+++ Controller/taskController.js
@@ -1,20 +1,26 @@
-const task = require('../Model/taskModel');
-const Project = require('../Model/projectModel')
+const task = require("../Model/taskModel");
+const Project = require("../Model/projectModel");
 
 const addTask = async (req, res) => {
-    const newtask = await task.create({...req.body.new,status:"pending"});
-    let newTasksArr = [...req.body.prev,newtask]
-    if (newtask) {
-        await Project.findByIdAndUpdate(req.body.new.project, {"task":newTasksArr}, {new: true}).then(data => {
-            res.json(data).status(200)
-        }).catch(error => {
-            res.json({message: error.message});
-        });
-    } else {    
-        res.status(401).send({
-            message: 'Something went wrong'
-        });
-    };
+  const newtask = await task.create({ ...req.body.new, status: "pending" });
+  let newTasksArr = [...req.body.prev, newtask];
+  if (newtask) {
+    await Project.findByIdAndUpdate(
+      req.body.new.project,
+      { task: newTasksArr },
+      { new: true }
+    )
+      .then((data) => {
+        res.json(data).status(200);
+      })
+      .catch((error) => {
+        res.json({ message: error.message });
+      });
+  } else {
+    res.status(401).send({
+      message: "Something went wrong",
+    });
+  }
 };
 
-module.exports = { addTask }
+module.exports = { addTask };
