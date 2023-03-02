8aeb736ee0b198e4e98ed85f2973ed8a85af34e3
Avesh-2712 <114075285+Avesh-2712@users.noreply.github.com>
2023-03-02 10:22:49 +0530

    Modified assTask API

--- Controller/taskController.js
+++ Controller/taskController.js
@@ -1,14 +1,20 @@
 const task = require('../Model/taskModel');
+const Project = require('../Model/projectModel')
 
 const addTask = async (req, res) => {
-    const { title, lead, assigned, status } = req.body;
-
-    await task.create({title, lead, assigned, status}).then(data => {
-        res.json(data).status(200);
-    }).catch(error => {
-        res.json({message: error.message}).status(500);
-    })
-}
-
+    const { project, title, lead, assigned, status } = req.body;
+    const tasks = await task.create({ project, title, lead, assigned, status});
+    if (tasks) {
+        await Project.findByIdAndUpdate(project, {"task":tasks}, {new: true}).then(data => {
+            res.json(data).status(200)
+        }).catch(error => {
+            res.json({message: error.message});
+        });
+    } else {    
+        res.status(401).send({
+            message: 'Something went wrong'
+        });
+    };
+};
 
 module.exports = { addTask }
