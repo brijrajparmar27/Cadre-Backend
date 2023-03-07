a883a03300632c2b1e09880c2b8bb410d8b06496
brijraj <brijrajparmar27@gmail.com>
2023-03-07 10:31:27 +0530

    change description instad of lead

--- Controller/taskController.js
+++ Controller/taskController.js
@@ -3,10 +3,14 @@ const Project = require("../Model/projectModel");
 
 const addTask = async (req, res) => {
   const newtask = await task.create({ ...req.body.new, status: "Pending" });
-  let newTasksArr = [...req.body.prev,newtask];
+  let newTasksArr = [...req.body.prev, newtask];
   if (newtask) {
-    await Project.findByIdAndUpdate( req.body.new.project, { task: newTasksArr }, { new: true });
-    res.status(200).json(newtask)
+    await Project.findByIdAndUpdate(
+      req.body.new.project,
+      { task: newTasksArr },
+      { new: true }
+    );
+    res.status(200).json(newtask);
   } else {
     res.status(401).send({
       message: "Something went wrong",
@@ -16,9 +20,13 @@ const addTask = async (req, res) => {
 
 const updateTask = async (req, res) => {
   const { id } = req.params;
-  const { project, title, lead, assigned, status } = req.body
-   await task
-    .findByIdAndUpdate(id, { project, title, lead, assigned, status }, { new: true })
+  const { project, title, lead, assigned, status } = req.body;
+  await task
+    .findByIdAndUpdate(
+      id,
+      { project, title, lead, assigned, status },
+      { new: true }
+    )
     .then((val) => {
       res.json(val).status(200);
     })
